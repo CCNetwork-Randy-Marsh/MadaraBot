e37f00d92cf236e323ee06da766860e8b52c5311
Lodberg1 <lodberglars@gmail.com>
2019-05-06 05:02:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,39 +14,13 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === 'Tis') {
 
-       message.reply('pong');
+       message.reply('Fucking Lort');
 
        }
 
 });
-
-    // Return Statements
-    if (message.author.bot) return; 
-    if (!message.content.startsWith(prefix)) return; 
-
-    // Command Handler
-    try {
-
-        // Bonus: Auto-Reload (You should move this into it's own command)
-        delete require.cache[require.resolve(`./commands/${cmd}.js`)]; 
-
-        // Options
-        let ops = {
-            ownerID: ownerID
-        }
-
-        let commandFile = require(`./commands/${cmd}.js`); 
-        commandFile.run(client, message, args, ops); 
-
-    } catch (e) { 
-        console.log(e.stack);
-    }
-
-});
-
-
 
 // THIS  MUST  BE  THIS  WAY
 
