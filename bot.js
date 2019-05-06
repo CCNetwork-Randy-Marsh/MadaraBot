d37c02b5193c2607afac9a55615cfb1589e36453
Lodberg1 <lodberglars@gmail.com>
2019-05-06 03:53:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client(); 
 
 // Constant Variables
-const prefix = process.env.PREFIX; 
-const ownerID = process.env.OWNER; 
+let prefix = process.env.PREFIX; 
 
 // Listener Events
 client.on('message', message => {
@@ -13,9 +12,6 @@ client.on('message', message => {
     let args = message.content.slice(prefix.length).trim().split(' ');
     let cmd = args.shift().toLowerCase();
 
-    // Now, we have two variables.  cmd contains the command following the prefix
-    // args contains everything following that and splits it into an array by slices
-
     // Return Statements
     if (message.author.bot) return; 
     if (!message.content.startsWith(prefix)) return; 
@@ -23,25 +19,16 @@ client.on('message', message => {
     // Command Handler
     try {
 
-        // Bonus: Auto-Reload (You should move this into it's own command)
-        delete require.cache[require.resolve(`./commands/${cmd}.js`)]; 
-
-        // Options
-        let ops = {
-            ownerID: ownerID
-        }
-
         let commandFile = require(`./commands/${cmd}.js`); 
-        commandFile.run(client, message, args, ops); 
+        commandFile.run(client, message, args); 
 
-    } catch (e) { 
+    } catch (e) {
         console.log(e.stack);
     }
 
 });
 
-// Ready Event - Bot Online / Bot started
+// Ready Event - Bot online / Bot started
 client.on('ready', () => console.log('Bot Launched!'));
 
-// Discord Login 
 client.login(process.env.TOKEN); 
