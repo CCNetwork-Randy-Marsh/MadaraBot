ad5f7a457030dea071f7c32c6f7393e87bc3a8f8
Lodberg1 <lodberglars@gmail.com>
2019-05-06 03:49:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,47 @@
+// Require Packages
 const Discord = require('discord.js');
+const client = new Discord.Client(); 
 
-const client = new Discord.Client();
+// Constant Variables
+const prefix = process.env.PREFIX; 
+const ownerID = process.env.OWNER; 
 
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
-});
-
- 
-
+// Listener Events
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    // Variables
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLowerCase();
 
-       message.reply('pong');
+    // Now, we have two variables.  cmd contains the command following the prefix
+    // args contains everything following that and splits it into an array by slices
 
-       }
+    // Return Statements
+    if (message.author.bot) return; 
+    if (!message.content.startsWith(prefix)) return; 
+
+    // Command Handler
+    try {
+
+        // Bonus: Auto-Reload (You should move this into it's own command)
+        delete require.cache[require.resolve(`./commands/${cmd}.js`)]; 
+
+        // Options
+        let ops = {
+            ownerID: ownerID
+        }
+
+        let commandFile = require(`./commands/${cmd}.js`); 
+        commandFile.run(client, message, args, ops); 
+
+    } catch (e) { 
+        console.log(e.stack);
+    }
 
 });
 
- 
+// Ready Event - Bot Online / Bot started
+client.on('ready', () => console.log('Bot Launched!'));
 
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//
+// Discord Login 
+client.login(process.env.TOKEN); 
